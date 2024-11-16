c8b16fb8207c78d8d68138390885206b6c8cbd2c
ramhofi <ram.hofi@post.runi.ac.il>
2024-11-16 22:21:05 +0200

    try6

--- Cheers.java
+++ Cheers.java
@@ -2,5 +2,24 @@
 public class Cheers {
         public static void main(String[] args) {
 	    // Replace this comment with your code
+            String cheer = args[0];
+            String anLetters = "AEFHILMNORSX";
+            for (int i = 0; i < cheer.length(); i++){
+                char c = cheer.charAt(i);
+                if (anLetters.indexOf(c) != -1) {
+                        System.out.println("Give me an " + c + ": " + c);
+                } else {
+                        System.out.println("Give me a " + c + ": " + c);
+                }
+            }
+                
+                  System.out.println("What" + " does" + " that" + " spell?");
+
+                  int number = Integer.parseInt(args[1]);
+                  
+                  for (int count = 0; count < number; count++) {
+                        System.out.println(cheer + "!!!");
+                 }            
+    
         }
 }
