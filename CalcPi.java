f41c7b601ab2ab8776b08e87385a2903948fd484
ramhofi <ram.hofi@post.runi.ac.il>
2024-11-17 01:14:07 +0200

    try13

--- CalcPi.java
+++ CalcPi.java
@@ -3,6 +3,7 @@ public class CalcPi {
 	public static void main(String [] args) { 
 	    // Replace this comment with your code
 		int calc = Integer.parseInt(args[0]);
+
 		double divider = 3.0;
 		double sum = 1.0;
 		int counter = 1;
@@ -10,9 +11,11 @@ public class CalcPi {
 
 		while (counter < calc) {
 			if (possitiveOrNegative == false){
+
 			sum = sum - (1.0 / divider);
 			possitiveOrNegative = true;	
 			} else {
+				
 				sum = sum + (1.0 / divider);
 				possitiveOrNegative = false;
 			}
@@ -20,7 +23,7 @@ public class CalcPi {
 			 divider = divider + 2.0;
 		}
 		System.out.println("pi according to Java: " + Math.PI);
-		System.out.println("pi according to Java:    " + 4.0 * sum);
+		System.out.println("pi according to Java:     " + sum * 4.0);
 
 	}
 }
